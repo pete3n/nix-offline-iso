6dd16a6e9634d61788096d3b7f2c2ca07af40a2f
pete3n <pete3n@protonmail.com>
2024-12-13 10:52:58 -0500

    fix: pin calamares-nixos-extensions to v0.3.14 pre luks2 patch

--- flake.nix
+++ flake.nix
@@ -3,60 +3,69 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    systems = [
-      "aarch64-linux"
-      "x86_64-linux"
-    ];
+  outputs =
+    { nixpkgs, ... }@inputs:
+    let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
 
-    in rec {
-    nixosConfigurationsForAllSystems = system: {
-      "offline-installer-${system}" = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        inherit system;
-        modules = [
-          ./nix-cfg/configuration.nix
-          ({ pkgs, lib, ... }: {
-            nixpkgs.overlays = [
-              (self: super: {
-                calamares-nixos-extensions = super.calamares-nixos-extensions.overrideAttrs (oldAttrs: rec {
-                  patches = oldAttrs.patches or [] ++ [ 
-                    ./patches/welcome.patch 
-                    ./patches/nixos.patch
+    in
+    rec {
+      nixosConfigurationsForAllSystems = system: {
+        "offline-installer-${system}" = nixpkgs.lib.nixosSystem {
+          specialArgs = {
+            inherit inputs;
+          };
+          inherit system;
+          modules = [
+            ./nix-cfg/configuration.nix
+            {
+              nixpkgs.overlays = [
+                (self: super: {
+                  calamares-nixos-extensions = super.calamares-nixos-extensions.overrideAttrs (oldAttrs: {
+                    src = super.fetchFromGitHub {
+                      owner = "NixOS";
+                      repo = "calamares-nixos-extensions";
+                      rev = "0.3.14";
+                      hash = "sha256-wRKZ80yU3WsUkGn5/ft4wDR22s39/WTxTrjFE0/9mlc=";
+                    };
+                    patches = oldAttrs.patches or [ ] ++ [
+                      ./patches/welcome.patch
+                      ./patches/nixos.patch
+                    ];
+                  });
+                })
+              ];
+            }
+            "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
+            (
+              { config, ... }:
+              {
+                isoImage = {
+                  contents = [
+                    {
+                      source = ./nix-cfg;
+                      target = "/nix-cfg";
+                    }
                   ];
-                });
-              })
-            ];
-          })
-          "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-graphical-calamares-gnome.nix"
-          ({ pkgs, config, ... }: {
-            isoImage = {
-              contents = [
-                {
-                    source = ./nix-cfg;
-                    target = "/nix-cfg";
-                }
-              ];
-              storeContents = [ 
-                config.system.build.toplevel
-              ];
-              includeSystemBuildDependencies = true;
-              squashfsCompression = "gzip -Xcompression-level 1";
-            };
-          })
-        ];
+                  storeContents = [ config.system.build.toplevel ];
+                  includeSystemBuildDependencies = true;
+                  squashfsCompression = "gzip -Xcompression-level 1";
+                };
+              }
+            )
+          ];
+        };
       };
+
+      # Generate nixosConfigurations for each system
+      nixosConfigurations = builtins.foldl' (
+        acc: system: acc // (nixosConfigurationsForAllSystems system)
+      ) { } systems;
+
+      # Generate iso configurations for each system
+      iso = builtins.mapAttrs (name: config: config.config.system.build.isoImage) nixosConfigurations;
     };
-
-    # Generate nixosConfigurations for each system
-    nixosConfigurations = builtins.foldl' (acc: system: acc // (nixosConfigurationsForAllSystems system)) { } systems;
-
-    # Generate iso configurations for each system
-    iso = builtins.mapAttrs (name: config: config.config.system.build.isoImage) nixosConfigurations;
-  };
 }
